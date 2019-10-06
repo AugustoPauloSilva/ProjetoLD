44502e2be117a975b34ba6f545f60cdd4c148ed2
AugustoPaulo <augustopaulo123@hotmail.com>
2019-10-06 18:15:01 -0300

    Enemy1 Behavior Fix

--- Assets/Scripts/Enemy1Behavior.cs
+++ Assets/Scripts/Enemy1Behavior.cs
@@ -6,7 +6,9 @@ public class Enemy1Behavior : MonoBehaviour
 {
     public IsOnGround groundRight;
     public IsOnGround groundLeft;
-    public float speed = 10f;
+    public float speed = 50f;
+    public float turnTimer = 15f;
+    float turnCount = 0f;
     Rigidbody2D body;
 
     void Start()
@@ -16,14 +18,27 @@ public class Enemy1Behavior : MonoBehaviour
 
     void Update()
     {
-        if(!groundRight.isGrounded || !groundLeft.isGrounded)
+
+    }
+
+    void FixedUpdate()
+    {
+        if(!groundRight.isGrounded || !groundLeft.isGrounded && turnCount == 0){
             speed = -speed;
-        body.velocity = new Vector2(speed,body.velocity.y);
+            turnCount = turnTimer;
+        }
+        if (turnCount > 0) turnCount--;
+        body.velocity = new Vector2(speed*Time.fixedDeltaTime,body.velocity.y);
     }
 
     void OnCollisionEnter2D(Collision2D other) {
-        if(other.gameObject.tag == "Play")
+        if(other.gameObject.tag == "Play"){
             other.gameObject.GetComponent<PlayerInput>().TakeDamage();
+            if (speed>0 && (Vector2.Angle(other.GetContact(0).normal,Vector2.right)<10f))
+                speed = -speed;
+            else if (speed<0 && (Vector2.Angle(other.GetContact(0).normal,Vector2.left)<10f))
+                speed = -speed;
+        }
         if(Vector2.Angle(other.GetContact(0).normal,Vector2.up) >= 10f) 
             speed = -speed;
     }
