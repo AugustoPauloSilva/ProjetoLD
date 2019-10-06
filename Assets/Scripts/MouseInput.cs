1cb9a20c4a75dfe8a99608cee95ffc47a72a54fb
bidigar <38304858+bidigar@users.noreply.github.com>
2019-10-06 19:37:28 -0300

    Add files via upload

--- Assets/Scripts/MouseInput.cs
+++ Assets/Scripts/MouseInput.cs
@@ -4,29 +4,21 @@ using UnityEngine;
 
 public class MouseInput : MonoBehaviour
 {
-    bool isOver = false;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(gameObject.tag == "Drag" && Input.GetMouseButtonDown(1)){
-            if (!isOver) GetComponent<ClickDrag>().clickReaction2();
-            else isOver = false;
-        }
+
     }
 
-    void OnMouseOver() {
-        if (gameObject.tag == "Destructible" && Input.GetMouseButtonDown(0)){
+    void OnMouseDown() {
+        if (gameObject.tag == "Obstacle"){
             GetComponent<ClickObstacle>().clickReaction();
-        }
-        else if (gameObject.tag == "Drag" && Input.GetMouseButtonDown(1)){
-            GetComponent<ClickDrag>().clickReaction();
-            isOver = true;
         }
     }
 }
